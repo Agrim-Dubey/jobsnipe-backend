6f981fc5adca76ed937c9f9c376b8953272ac8e8
Agrim <agrimdubey37@gmail.com>
2026-03-20 09:24:19 +0530

    Added the user model

---
+++ fastapi_app/models/users.py
@@ -0,0 +1,11 @@
+from fastapi_app.db.database import Base
+from sqlalchemy import Column, Integer,String,DateTime,Boolean
+from datetime import datetime,timezone
+
+class User(Base):
+    __tablename__="users"
+    id=Column(Integer,primary_key=True,index=True)
+    email=Column(String,unique=True,index=True,nullable=False)
+    hashed_password=Column(String,nullable=False)
+    created_at=Column(DateTime,default=lambda: datetime.now(timezone.utc))
+    is_active=Column(Boolean,default=False)
