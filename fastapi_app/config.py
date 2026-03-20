18cae8e9c1d959776ea3a53c164fb6bc2fdb1a07
Agrim <agrimdubey37@gmail.com>
2026-03-20 10:02:17 +0530

    Added the S3 client for resume storage

--- fastapi_app/config.py
+++ fastapi_app/config.py
@@ -8,7 +8,11 @@ class Settings(BaseSettings):
     POSTGRES_USER:str
     ACCESS_TOKEN_EXPIRE_MINUTES:int
     REFRESH_TOKEN_EXPIRE_MINUTES:int
-
+    AWS_ACCESS_KEY_ID:str
+    AWS_SECRET_ACCESS_KEY1:str
+    AWS_S3_BUCKET_NAME:str
+    AWS_REGION_NAME:str
+    
     model_config = SettingsConfigDict(env_file=".env")
     
 
---
+++ fastapi_app/utils/s3.py
@@ -0,0 +1,23 @@
+import boto3
+from botocore.exceptions import ClientError
+from fastapi_app.config import settings 
+import uuid
+from fastapi import HTTPException
+
+
+s3_client = boto3.client("s3",aws_access_key_id=settings.AWS_ACCESS_KEY_ID,aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY1,region_name=settings.AWS_REGION_NAME)
+
+def upload_resume_to_s3(file_bytes,original_filename,user_id):
+    file_extension=original_filename.split(".")[-1]
+    unique_filename=f"resumes/{user_id}/{uuid.uuid4()}.{file_extension}"
+    try:
+        s3_client.put_object(Bucket=settings.AWS_S3_BUCKET_NAME,Key=unique_filename,Body=file_bytes,ContentType="application/pdf")
+        return unique_filename
+    except ClientError as e:
+        raise HTTPException(status_code=500,detail="Failed to upload resume to s3")
+    
+    
+def get_S3_link(filename):
+    return f"https://{settings.AWS_S3_BUCKET_NAME}.s3.{settings.AWS_REGION_NAME}.amazonaws.com/{filename}"
+
+
