3f7a0c0673de970c0e52e2756f2b51614269fbb6
Agrim <agrimdubey37@gmail.com>
2026-03-20 09:12:08 +0530

    Added the basic settings config structure

---
+++ fastapi_app/config.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+
+class Settings(BaseSettings):
+    SECRET_KEY:str 
+    DATABASE_URL:str
+    POSTGRES_USER:str
+
+    model_config = SettingsConfigDict(env_file=".env")
+    
+
+
+
+
+settings = Settings()
+
